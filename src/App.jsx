6f6507523e4a7d9fd8c919f74eefb0335ca96a4d
Abdulloh <abdulloh@example.com>
2026-06-30 17:28:42 +0500

    Commit xabari

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from './companents/Header'
+import Hero from './companents/Hero'
 
 const App = () => {
   return (
     <div>
       <Header />
-      
+      <Hero />
     </div>
   )
 }
