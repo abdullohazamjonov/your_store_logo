a2a137b2620df1f01cc547d71eb62f6d37dd3f64
Abdulloh <abdulloh@example.com>
2026-07-02 15:30:24 +0500

    Commit xabari

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,6 +5,9 @@ import Sindebar from '../../components/Sidebar'
 import Card from '../../components/Card'
 import Input from '../../components/Input'
 import Test from '../../components/Test'
+import Tabs from '../../components/Tabs'
+import Products from '../../components/Products'
+import Grid from '../../components/Grid'
 
 const Home = () => {
   return (
@@ -14,6 +17,9 @@ const Home = () => {
       <Card />
       <Input />
       <Test />
+      <Tabs />
+      <Products />
+      <Grid />
     </>
   )
 }
